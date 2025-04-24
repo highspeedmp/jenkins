a7f34df922fa7e253b96c3ad17c5dcd32beb5fea
Matthew Promenchenkel <mattprom@gmail.com>
2025-04-24 15:12:32 +0000

    use wget to download jenkins.pp

--- jenkins.sh
+++ jenkins.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
+# This script checks for prerequisites, installs the puppet agent
+# and pulls down the puppet agent configuration file
 
 # Make sure the script is being run by the root user
 if [[ "$EUID" -ne 0 ]]; then
   echo "This script must be run as root."
   exit 1
 fi
-# Check for prerequisites, pull down installer script
-# not sure if we will use curl for single file, or pull down a git repo for this
-if command -v curl &> /dev/null; then
-  echo "curl is installed"
+# wget is installed in the base Ubuntu 22.04 install, double check anyway
+if command -v wget &> /dev/null; then
+  echo "wget is installed"
 else
-  apt install -y curl
+  apt install -y wget
 fi
 
-if command -v git &> /dev/null; then
-  echo "git is installed"
-else
-  apt install -y git
-fi
 if command -v puppet &> /dev/null; then
   echo "puppet is installed"
 else
   apt install -y puppet
 fi
 
-# Here we download our .pp file then do a puppet apply jenkins.pp
+# Here we download our .pp file from my github gist then do a puppet apply jenkins.pp
+cd ~
+wget -O jenkins.pp https://gist.githubusercontent.com/highspeedmp/8163ce1e16dd221497ed22a5217f311c/raw/8405020bdbdbac2333c4036b4068e2e439fce46c/jenkins.pp
+puppet apply jenkins.pp
